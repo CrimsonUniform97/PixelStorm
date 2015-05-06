c15b3da07edf979722db2f52030038685566514a
CrazyPyroEagle <crazypyroeagle@gmail.com>
2015-05-06 16:16:02 +0200

    Change Sponge API version to 2.1-SNAPSHOT & add main class

--- src/pyroscourge/pixelauc/PixelmonAuctions.java
+++ src/pyroscourge/pixelauc/PixelmonAuctions.java
@@ -3,13 +3,9 @@ package pyroscourge.pixelauc;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.event.Subscribe;
-import org.spongepowered.api.event.entity.player.PlayerJoinEvent;
 import org.spongepowered.api.event.state.InitializationEvent;
+import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.service.command.CommandService;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.format.TextColors;
-import org.spongepowered.api.text.format.TextStyles;
-import org.spongepowered.api.text.title.Title;
 import org.spongepowered.api.util.command.CommandCallable;
 import org.spongepowered.api.util.command.CommandMapping;
 
@@ -18,6 +14,7 @@ import pyroscourge.pixelauc.commands.AuctionCommand;
 import com.google.common.base.Optional;
 import com.google.inject.Inject;
 
+@Plugin(id = PixelmonAuctions.PLUGIN_ID, name = PixelmonAuctions.PLUGIN_NAME, version = PixelmonAuctions.PLUGIN_VERSION)
 public class PixelmonAuctions {
 	public static final String PLUGIN_ID = "pixelauc";
 	public static final String PLUGIN_NAME = "Pixelmon Auctions";
@@ -27,17 +24,13 @@ public class PixelmonAuctions {
 	private Logger logger;
 	@Inject
 	private Game game;
-	@Inject
 	private CommandService cmdService;
 	
 	@Subscribe
 	public void onInitialization(InitializationEvent event) {
+		logger.info("{} - Version {}", PLUGIN_NAME, PLUGIN_VERSION);
+		cmdService = game.getCommandDispatcher();
 		registerCommand(new AuctionCommand(game.getServer()), "auction", "auc");
-	}
-	
-	@Subscribe
-	public void onPlayerJoin(PlayerJoinEvent event) {
-		event.getPlayer().sendTitle(new Title(new Text.Literal(TextColors.AQUA, TextStyles.NONE, null, null, null, null, "Welcome!"), new Text.Literal(TextColors.GOLD, TextStyles.NONE, null, null, null, null, "Day " + event.getPlayer().getWorld().getWeather().getName()), 20, 100, 20, true, false));
 	}
 	
 	private Optional<CommandMapping> registerCommand(CommandCallable command, String... alias) {
